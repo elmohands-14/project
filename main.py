823c5dbb8911941532c9501b9b0c9cc98f522ca2
Salah Ali <salahspider14@gmail.com>
2023-07-01 14:31:26 +0300

    step 1

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import ttk
+
+
+root = Tk()
+root.title("Employee Management System")
+root.geometry("1310x515+20+100")
+root.resizable(False,False)
+root.configure(bg="#2c3e50")
+
+
+
+root.mainloop()
